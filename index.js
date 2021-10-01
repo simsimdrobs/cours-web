0f51358b8eebd376747594137e850dab12ac326d
Simon Drobinski <simon.drobinski@hotmail.fr>
2021-10-01 13:23:32 +0200

    fix: modified index.js file to use express for server

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const http = require('http');
+const path = require('path')
+
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+app.use("/static", express.static(path.join(__dirname, '/static')))
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => {
+    res.redirect(301, '/static/index.html')
+})
+
+
+app.use(function (req, res) {
+    console.log("et c'est le 404 : " + req.url);
+
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+
+    res.end("<html><head><title>la quatre cent quatre</title></head><body><h1>Et c'est la 404.</h1><img  src=\"https://www.leblogauto.com/wp-content/uploads/2020/04/Peugeot-404-1.jpg\" /></body></html>");
+
+})
+
+app.listen(port, hostname);
+console.log(`Server running at http://${hostname}:${port}/`);
