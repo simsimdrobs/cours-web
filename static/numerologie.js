4287122255d53da080487395de623e630bc13145
Simon Drobinski <simon.drobinski@hotmail.fr>
2021-10-01 13:18:38 +0200

    feature: put front files in static folder

---
+++ static/numerologie.js
@@ -0,0 +1,42 @@
+function nombre(chaine) {
+    var somme = 0
+    for (var i=0; i < chaine.length; i++) {
+        somme += chaine.charCodeAt(i)
+    }
+    return somme
+}
+
+// test de nombre(chaine)
+
+// est-ce 2x plus ?
+console.log(nombre("cou"))
+console.log(nombre("coucou"))
+
+// chaque caractère :la somme est-elle correcte ?
+for (c of "cou") { 
+    console.log(c + " : " + nombre(c))
+}
+// fin de test de nombre(chaine)
+
+function somme(nombre) {
+    var somme = 0
+    chaine = String(nombre)
+    for (var i=0; i < chaine.length ; i++) {
+        somme += parseInt(chaine.charAt(i))
+    }
+    return somme
+}
+
+// test de somme(nombre)
+console.log(somme(132))
+
+// avec un chiffre : charAt != charCodeAt
+console.log(somme(4))
+console.log("4".charCodeAt(0))
+console.log("4".charAt(0))
+
+// conversion chaine de caracteres et nombre
+console.log(typeof "4".charAt(0))
+console.log(parseInt("4".charAt(0)))
+console.log(typeof parseInt("4".charAt(0)))
+// fin de test de somme(nombre)
